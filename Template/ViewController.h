daccff17b80445ec7c2e03fe5ac8ed652675b4fd
张冬冬 <godzzzzz@qq.com>
2019-02-18 14:40:02 +0800

    基本框架

---
+++ Template/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Template
+//
+//  Created by 张冬冬 on 2019/2/18.
+//  Copyright © 2019 binary. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Template/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Template
+//
+//  Created by 张冬冬 on 2019/2/18.
+//  Copyright © 2019 binary. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
